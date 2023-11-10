2fbaeeb809dd62e6822a67a6190f67e900d840bf
rafifditya <rafifabdurrahman12@gmail.com>
2023-11-10 19:24:10 +0700

    update back end

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
     <?php 
+    
+        $array = array(
+            "0" => array('PASLON1' => rand_float(0,100)."",
+                        'PASLON2' => rand_float(0,100)."",
+                        'PASLON3'=> rand_float(0,100)."",
+                        'TOTAL'=> rand_float(0,100).""
+        ));
+        $json = json_encode($array);
+        echo $json;
         function rand_float($st_num=0,$end_num=1,$mul=1000000)
         {
         if ($st_num>$end_num) return false;
         return mt_rand($st_num*$mul,$end_num*$mul)/$mul;
         }
-     $array = array(
-        "0" => array('PASLON1' => rand_float(0,100,3)."",
-                    'PASLON2' => rand_float(0,100,3)."",
-                    'PASLON3'=> rand_float(0,100,3)."",
-                    'TOTAL'=> rand_float(0,100,3).""
-    ));
-    $json = json_encode($array);
-    echo $json;
+    //  $array = array(
+    //     "0" => array('PASLON1' => rand_float(0,100)."",
+    //                 'PASLON2' => rand_float(0,100)."",
+    //                 'PASLON3'=> rand_float(0,100)."",
+    //                 'TOTAL'=> rand_float(0,100).""
+    // ));
+    // $json = json_encode($array);
+    // echo $json;
     
-    $path = 'pilpres.json';
-    $fp = fopen($path, 'w');
-    fwrite($fp, $json);
-    fclose($fp);
+    // $path = 'pilpres.json';
+    // $fp = fopen($path, 'w');
+    // fwrite($fp, $json);
+    // fclose($fp);
     ?>
