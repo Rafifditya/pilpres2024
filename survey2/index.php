c79b4d5e255441324d2f7bcf26ddc1533aa27789
rafifditya <rafifabdurrahman12@gmail.com>
2024-01-11 18:49:27 +0700

    [update] full with json and xml

--- survey2/index.php
+++ survey2/index.php
@@ -71,6 +71,37 @@
             return $array;
         }
 
+        function getParnasZon(){
+            $xml=simplexml_load_file("partai_nasional_perzonasi.xml") or die("Error: Cannot create object");
+            $array = [];
+            foreach($xml->data->item as $row){
+                $array[] = array(
+                    'namazonasi' => $row->namazonasi .'',
+                    'partai1' => $row->partai1 .'',
+                    'partai2' => $row->partai2 .'',
+                    'partai3' => $row->partai3 .'',
+                    'partai4' => $row->partai4 .'',
+                    'partai5' => $row->partai5 .'',
+                    'partai6' => $row->partai6 .'',
+                    'partai7' => $row->partai7 .'',
+                    'partai8' => $row->partai8 .'',
+                    'partai9' => $row->partai9 .'',
+                    'partai10' => $row->partai10 .'',
+                    'partai11' => $row->partai11 .'',
+                    'partai12' => $row->partai12 .'',
+                    'partai13' => $row->partai13 .'',
+                    'partai14' => $row->partai14 .'',
+                    'partai15' => $row->partai15 .'',
+                    'partai16' => $row->partai16 .'',
+                    'partai17' => $row->partai17 .'',
+                    'partai24' => $row->partai24 .'',
+                    'datamasuk' => $row->datamasuk .'',
+                    'tingkatpartisipasi' => $row->tingkatpartisipasi .''
+                );
+            }
+            return $array;
+        }
+
         function getPilnaspro(){
             $xml=simplexml_load_file("pilpres_nasional_perprovinsi.xml") or die("Error: Cannot create object");
             $array = [];
@@ -89,12 +120,34 @@
             );
             return $array;
         }
+
+        function getPilnasZon(){
+            $xml=simplexml_load_file("pilpres_nasional_perzonasi.xml") or die("Error: Cannot create object");
+            $array = [];
+            foreach($xml->data->item as $row){
+                $array[] = array(
+                    'namazonasi' => $row->namazonasi .'',
+                    'paslon1' => $row->paslon1 .'',
+                    'paslon2' => $row->paslon2 .'',
+                    'paslon3' => $row->paslon3 .'',
+                    'datamasuk' => $row->datamasuk. '',
+                    'tingkatpartisipasi' => $row->tingkatpartisipasi .''
+                );
+            }
+            $result = array (
+                'data' => $array
+            );
+            return $array;
+        }
+        
         
         $result = array(
          'pilpres_nasional' => getPilnas(),
          'partai_nasional' => getParnas(),
          'pilpres_nasional_perprovinsi' => getPilnaspro(),
          'partai_nasional_perprovinsi' => getParnaspro(),
+         'pilpres_nasional_perzonasi' => getPilnasZon(),
+         'partai_nasional_perzonasi' => getParnasZon()
         );
         $json = json_encode($result,JSON_PRETTY_PRINT);
         echo $json; 
